afe7bbd48d23381daaf973ea99281545caab629e
BK <biskuitgem@gmail.com>
2021-02-28 01:03:44 +0800

    updated about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Page from "../components/Page";
+import gems from "../assets/img/gems.png";
 
 const AboutPage = () => {
     return (
@@ -9,6 +10,9 @@ const AboutPage = () => {
                     <p>Here is a little history on how we came up with our name “BiskuitGem”. Do a quick google on “iced gem biscuit” and you will find this colourful snack. This is one of our all-time favourite snack when we were young. To us, this is a nostalgic and colourful snack which we love. If you have yet to try it, do get it! Its a simple and delicious snack which never fails to bring a smile to our face!</p>
                     <p>As developers, not only do we want to enjoy developing products and solutions, we also want people who use our products to enjoy them as much as we do. At BiskuitGem, we want our developers to enjoy the little things in life which makes us happy. Memories of this snack popped up in our mind as we were finding stuff which makes us happy. Hence, this is how we decided on our name!</p>
                 </div>
+            </div>
+            <div className="row">
+                <img src={gems} alt="gems" width="100%" height="auto" />
             </div>
         </Page>
     );
